70afca24b4391993670dc0da7ee7de9a9a255394
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-11-29 04:06:22 +0000

    Added foriegn keys for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_29_035409) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_29_040439) do
   create_table "boards", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
