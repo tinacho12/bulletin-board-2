19c0fc4ecf03607d0052a113aa99ba7ef0ba5ad9
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-11-29 04:20:53 +0000

    Updated dev.rake

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_29_040439) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_29_040600) do
   create_table "boards", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_29_040439) do
     t.integer "board_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
